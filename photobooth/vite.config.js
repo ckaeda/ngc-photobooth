34302420e23be29f2fa89e1b5c5d6165cb82c5dd
ckaeda <iamgpmc@gmail.com>
2025-04-27 02:04:27 +0800

    enabled https

--- photobooth/vite.config.js
+++ photobooth/vite.config.js
@@ -1,7 +1,14 @@
+import fs from 'fs'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('./certs/cert.key'),
+      cert: fs.readFileSync('./certs/cert.crt')
+    },
+    host: true,
+  },
 })
