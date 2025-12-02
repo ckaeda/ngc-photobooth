97273bf528105ca6b523e20ccae0375b95e1da81
ckaeda <iamgpmc@gmail.com>
2025-12-02 20:55:55 +0800

    CONFIG NANAMAN

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 export const GLOBAL = {
     ASPECT_RATIO_STR: "16 / 9",
     ASPECT_RATIO: 16 / 9,
-    IMAGE_COUNTDOWN: 0,
-    CAPTURE_COUNTDOWN: 0,
+    IMAGE_COUNTDOWN: 5,
+    CAPTURE_COUNTDOWN: 2,
     IMAGE_FORMAT: 'image/png',
     MAX_PHOTOS: 3
 };
